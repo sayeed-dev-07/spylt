4c6eedd1eb886b0da6406ce18ea98e877f05fc2a
sayeed <expsayeedshorif@gmail.com>
2026-01-24 18:40:55 +0600

    added mask CardReveal

--- components/BenefitSection.tsx
+++ components/BenefitSection.tsx
@@ -4,10 +4,17 @@ import TextAnimation from './textReveal';
 
 const BenefitSection = () => {
     return (
-        <div className='bg-black min-h-dvh '>
-            <TextAnimation style='text-xl text-milk max-w-[450px] mx-auto text-center' text={`Unlock the Advantages:
+        <div className='bg-black min-h-dvh py-[5%] flex flex-col items-center  gap-y-6 sm:gap-y-12 md:gap-y-24'>
+            <TextAnimation style='text-lg
+            px-1.5 sm:text-2xl text-milk max-w-[600px] mx-auto text-center' text={`Unlock the Advantages:
                 Explore the Key Benefits of Choosing SPYLT`}/>
-            {/* <CardReveal/> */}
+            <div>
+                <CardReveal text='Shelf stable' bgColor='#c88e64' rotate='rotate-3'/>
+            <CardReveal text='Protein + Caffeine' bgColor='#faeade' white={false} rotate='rotate-0'/>
+            <CardReveal text='Infinitely recyclable' bgColor='#7f3b2d' rotate='rotate-2'/>
+            <CardReveal text='Lactose free' bgColor='#b59b5a' white={false} rotate='-rotate-6'/>
+            </div>
+
         </div>
     );
 };
--- components/CardReveal.tsx
+++ components/CardReveal.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+'use client'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useRef } from 'react';
 
-interface prop{
+interface prop {
     text: string,
     white?: boolean,
-    bgColor:string
+    bgColor: string,
+    rotate?: string
 }
 
-const CardReveal = ({text, white=true, bgColor}:prop) => {
-    return (
-        <div>
+gsap.registerPlugin(ScrollTrigger)
+
+const CardReveal = ({ text, white = true, bgColor, rotate }: prop) => {
+
+    const cardRef = useRef<HTMLDivElement | null>(null)
+
+    useGSAP(() => {
+        gsap.timeline({
+            scrollTrigger:{
+                trigger:cardRef.current,
+                start:'top 60%',
+                end:'bottom 30%',
+                scrub:0.5,
+            }
             
+        })
+            .to(cardRef.current, {
+                clipPath: ' polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+                ease: 'power3.out',
+            })
+
+    })
+
+    return (
+        <div style={{ clipPath: 'polygon(49% 0, 49% 0, 50% 100%, 50% 100%)', willChange: 'clip-path' }} ref={cardRef} className={`bg-black p-1 sm:p-3  w-fit mx-auto ${rotate}`}>
+            <div style={{ backgroundColor: bgColor }} className={`${white ? 'text-milk' : 'text-black'}  text-5xl md:text-8xl  xl:text-[150px] text-center font-antonio uppercase font-bold tracking-tighter pb-5 py-3 px-3 sm:px-14 `}>
+                {text}
+            </div>
         </div>
     );
 };
